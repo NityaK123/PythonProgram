3564a627769b677d80cf330737a2b1ddc8a3e00a
Nityanand Gupta <54433920+NityaK123@users.noreply.github.com>
2019-08-28 01:29:00 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,20 @@
+x=int(input("Enter number of input:"))
+i=0
+y=[]
+while x>i:
+	r=y.insert(i,int(input()))
+	i=i+1
+print(y)
+for j in y:
+	for i in range(1,11):
+		s=i*j
+		print(s)
+	print("")
+		
+	
+	
+	
+
+
+
+	
